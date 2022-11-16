3f7a9db73b13d35dc22c33eccad61439e3334137
minhcuong2102 <cuong21022003@gmail.com>
2022-11-16 21:19:02 +0700

    nopbai

--- bai_3/thuc_hanh/truy_van_csdl_sinh_vien.sql
+++ bai_3/thuc_hanh/truy_van_csdl_sinh_vien.sql
@@ -5,3 +5,7 @@ select * from subject where credit < 10;
 select s.student_id, s.student_name, c.class_name
 from student s join class c on s.class_id = c.class_id
 where c.class_name = 'A1';
+
+select S.student_id, S.student_name, Sub.sub_name, M.Mark
+from student S join mark M on S.student_id = M.student_id join Subject Sub on M.sub_id = Sub.sub_id
+where Sub.sub_name = 'CF';
