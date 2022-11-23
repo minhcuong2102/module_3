b48056de9305e3f9c647df45d2ca33632f8ecfd6
minhcuong2102 <cuong21022003@gmail.com>
2022-11-23 21:16:38 +0700

    nopbai

--- case_study/task_3_query_co_ban/query_co_ban.sql
+++ case_study/task_3_query_co_ban/query_co_ban.sql
@@ -30,3 +30,12 @@ left join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
 left join loai_dich_vu ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
 group by dv.ten_dich_vu, kh.ho_ten;
 
+-- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu
+-- của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+from dich_vu dv join loai_dich_vu ldv on dv.ma_dich_vu = ldv.ma_loai_dich_vu
+join hop_dong hd on dv.ma_dich_vu = hd.ma_dich_vu
+where dv.ma_dich_vu not in (select dv.ma_dich_vu from hop_dong hd
+							join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
+							where month(hd.ngay_lam_hop_dong) between 1 and 3)
+group by dv.ma_dich_vu;
