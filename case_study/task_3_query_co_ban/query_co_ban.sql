629c370d71f869ba049a26ca0f34d69c86ee1229
minhcuong2102 <cuong21022003@gmail.com>
2022-11-25 21:28:54 +0700

    nopbai

--- case_study/task_3_query_co_ban/query_co_ban.sql
+++ case_study/task_3_query_co_ban/query_co_ban.sql
@@ -114,3 +114,11 @@ drop view so_lan_su_dung;
 
 -- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
 -- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+select hd.ma_hop_dong, ldv.ten_loai_dich_vu, dvdk.ten_dich_vu_di_kem, count(dvdk.ma_dich_vu_di_kem) as so_lan_su_dung from hop_dong hd
+join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
+join loai_dich_vu ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+group by dvdk.ma_dich_vu_di_kem
+having so_lan_su_dung = 1
+order by hd.ma_hop_dong;
