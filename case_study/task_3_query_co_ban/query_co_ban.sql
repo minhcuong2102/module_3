fd45c161f92a7451dcf81c9d8753cdebc89bbb97
minhcuong2102 <cuong21022003@gmail.com>
2022-11-25 21:04:42 +0700

    nopbai

--- case_study/task_3_query_co_ban/query_co_ban.sql
+++ case_study/task_3_query_co_ban/query_co_ban.sql
@@ -89,15 +89,16 @@ and (kh.dia_chi like '% Vinh' or kh.dia_chi like '% Quảng Ngãi');
 -- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ
 -- đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
 select hd.ma_hop_dong, nv.ho_ten as ho_ten_nhan_vien, kh.ho_ten as ho_ten_khach_hang, kh.so_dien_thoai as sdt_khach_hang, 
-dv.ma_dich_vu, dv.ten_dich_vu, ifnull(hdct.so_luong, 0) as so_luong_dich_vu_di_kem, hd.tien_dat_coc 
+dv.ma_dich_vu, dv.ten_dich_vu, sum(ifnull(hdct.so_luong, 0)) as so_luong_dich_vu_di_kem, hd.tien_dat_coc 
 from hop_dong hd
 join nhan_vien nv on hd.ma_nhan_vien = nv.ma_nhan_vien
 join khach_hang kh on hd.ma_khach_hang = kh.ma_khach_hang
 join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
 left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
 where (month(hd.ngay_lam_hop_dong) >= 10 and year(hd.ngay_lam_hop_dong) = 2020) and 
-	dv.ma_dich_vu not in(select hd.ma_dich_vu from hop_dong hd
-						where month(hd.ngay_lam_hop_dong) <= 6 and year(hd.ngay_lam_hop_dong) = 2021);
+		dv.ma_dich_vu not in(select hd.ma_dich_vu from hop_dong hd
+							where month(hd.ngay_lam_hop_dong) <= 6 and year(hd.ngay_lam_hop_dong) = 2021)
+group by hd.ma_hop_dong;
 
 -- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.
 -- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
