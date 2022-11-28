51ff7a2140964ab0421e27bee199d50acc493864
minhcuong2102 <cuong21022003@gmail.com>
2022-11-28 19:08:21 +0700

    nopbai

--- case_study/task_3_query_co_ban/query_co_ban.sql
+++ case_study/task_3_query_co_ban/query_co_ban.sql
@@ -136,11 +136,48 @@ order by ma_nhan_vien;
 
 -- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
 delete from nhan_vien
-where ma_nhan_vien not in 
-(
-	select nv.ma_nhan_vien from nhan_vien nv
-    join hop_dong hd on nv.ma_nhan_vien = hd.ma_nhan_vien
-    where year(hd.ngay_lam_hop_dong) between 2019 and 2021
-    group by nv.ma_nhan_vien
-);
+where ma_nhan_vien not in (select * from
+	(
+		select nv.ma_nhan_vien from nhan_vien nv
+		join hop_dong hd on nv.ma_nhan_vien = hd.ma_nhan_vien
+		where year(hd.ngay_lam_hop_dong) between 2019 and 2021
+		group by nv.ma_nhan_vien
+	)
+temp);
 
+-- 17.	Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum lên Diamond, chỉ cập nhật những khách hàng
+-- đã từng đặt phòng với Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ.
+update khach_hang set ma_loai_khach = 1
+where ma_khach_hang in (select * from 
+	(
+		select kh.ma_khach_hang from khach_hang kh
+		join hop_dong hd on hd.ma_khach_hang = kh.ma_khach_hang
+		join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
+		join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+		join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+		where year(hd.ngay_lam_hop_dong) = 2021 and kh.ma_loai_khach = 2
+		group by kh.ma_khach_hang
+		having sum(ifnull(dv.chi_phi_thue, 0) + ifnull(hdct.so_luong, 0) * ifnull(dvdk.gia, 0)) > 1000000
+    )
+temp);
+
+-- 18.	Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc giữa các bảng).
+set foreign_key_checks = 0;
+delete from khach_hang
+where ma_khach_hang in (select * from 
+	(
+		select kh.ma_khach_hang from khach_hang kh
+		join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
+        where year(ngay_lam_hop_dong) < 2021
+        group by kh.ma_khach_hang
+	)
+temp);
+set foreign_key_checks = 1;
+
+-- 19.	Cập nhật giá cho các dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2020 lên gấp đôi.
+update dich_vu_di_kem set gia = gia * 2
+where ma_dich_vu_di_kem in (select * from
+	(
+		
+	)
+temp);
