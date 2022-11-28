4714c693e6cdddc8f0ef7a7809f4d278f73b2da9
minhcuong2102 <cuong21022003@gmail.com>
2022-11-28 18:09:43 +0700

    nopbai

--- case_study/task_3_query_co_ban/query_co_ban.sql
+++ case_study/task_3_query_co_ban/query_co_ban.sql
@@ -122,3 +122,25 @@ join loai_dich_vu ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
 group by dvdk.ma_dich_vu_di_kem
 having so_lan_su_dung = 1
 order by hd.ma_hop_dong;
+
+-- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi
+-- mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+select nv.ma_nhan_vien, nv.ho_ten, td.ten_trinh_do, bp.ten_bo_phan, nv.so_dien_thoai, nv.dia_chi from nhan_vien nv
+join trinh_do td on nv.ma_trinh_do = td.ma_trinh_do
+join bo_phan bp on nv.ma_bo_phan = bp.ma_bo_phan
+join hop_dong hd on nv.ma_nhan_vien = hd.ma_nhan_vien
+where year(hd.ngay_lam_hop_dong) in (2020, 2021)
+group by nv.ma_nhan_vien
+having count(nv.ma_nhan_vien) <= 3
+order by ma_nhan_vien;
+
+-- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
+delete from nhan_vien
+where ma_nhan_vien not in 
+(
+	select nv.ma_nhan_vien from nhan_vien nv
+    join hop_dong hd on nv.ma_nhan_vien = hd.ma_nhan_vien
+    where year(hd.ngay_lam_hop_dong) between 2019 and 2021
+    group by nv.ma_nhan_vien
+);
+
