dba2f5ab1c06dfe4d78d6050ca47e9e83335fa63
minhcuong2102 <cuong21022003@gmail.com>
2022-12-23 18:55:31 +0700

    nopbai

--- bai_13/thuc_hanh/quan_ly_user_1/src/main/java/controller/UserServlet.java
+++ bai_13/thuc_hanh/quan_ly_user_1/src/main/java/controller/UserServlet.java
@@ -61,6 +61,12 @@ public class UserServlet extends HttpServlet {
                 case "permision":
                     addUserPermision(request, response);
                     break;
+                case "test-without-tran":
+                    testWithoutTran(request, response);
+                    break;
+                case "test-use-tran":
+                    testUseTran(request, response);
+                    break;
                 default:
                     listUser(request, response);
                     break;
@@ -68,6 +74,14 @@ public class UserServlet extends HttpServlet {
         } catch (SQLException ex) {
             throw new ServletException(ex);
         }
+    }
+
+    private void testUseTran(HttpServletRequest request, HttpServletResponse response) {
+        userDAO.insertUpdateUseTransaction();
+    }
+
+    private void testWithoutTran(HttpServletRequest request, HttpServletResponse response) {
+        userDAO.insertUpdateWithoutTransaction();
     }
 
     private void addUserPermision(HttpServletRequest request, HttpServletResponse response) {
